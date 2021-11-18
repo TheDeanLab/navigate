5828e18ffc46432688fbbe6f0dc5612aeb800eaf
Microscopy Innovation Lab <42547789+AdvancedImagingUTSW@users.noreply.github.com>
2021-11-18 15:08:25 -0600

    Minor changes

--- base/1.0.0/StartMonitor.py
+++ base/1.0.0/StartMonitor.py
@@ -7,7 +7,7 @@
         >>> config_file = 'config.yml'
         >>> startMonitor.start(config_dir,config_file)
 
-    The config file is passed to a :class:`~UUTrack.Model._session` variable. That will be shared with the rest of the program. The session variable idea comes from programming websites, but is useful in other kind of programs as well.
+    The config file is passed to a :class:`~UUTrack.Model.Session` variable. That will be shared with the rest of the program. The session variable idea comes from programming websites, but is useful in other kind of programs as well.
 
     :copyright: 2017
 
@@ -17,11 +17,7 @@ import os
 import sys
 from datetime import datetime
 
-from model.session import _session
-
-# Camera Imports
-from model.camera.dcam import Dcam
-from model.camera.dcam import Dcamapi
+from model.session import Session
 
 # Import View
 from view.main_application_window import Main_App as main_window
@@ -37,6 +33,7 @@ def start(constants, verbose=False):
     :param str configFile: Name of the config file
     :return: Window for the camera
     """
+    initialize_camera = False
 
     camera_config = constants.CameraParameters
     saving_config = constants.SavingParameters
@@ -44,7 +41,7 @@ def start(constants, verbose=False):
         print("The Camera Configuration is:", camera_config)
         print("The Saving Parameters are:", saving_config)
 
-    session = _session(camera_config.camera_parameters, saving_config, verbose)
+    #session = Session(camera_config.camera_parameters, saving_config, verbose)
 
     # Load the GUI
     # monitorMain(session)
@@ -61,36 +58,43 @@ def start(constants, verbose=False):
 
     session.Saving = {'directory': savedir}
     '''
-    
-    # Initialize Dcamapi
-    if Dcamapi.init() is not False:
-        n = Dcamapi.get_devicecount()
-        if verbose:
-            print("Found %d cameras" % n)
 
-        # Initialize the cameras
-        low_resolution_camera = Dcam(0)
-        high_resolution_camera = Dcam(1)
+    if initialize_camera == True:
+        # Camera Imports
+        from model.camera.dcam import Dcam
+        from model.camera.dcam import Dcamapi
 
-        # Confirm cameras are open, and set default parameters
-        if low_resolution_camera.dev_open() is not False:
-            low_resolution_camera.dcam_set_default_light_sheet_mode_parameters()
+        # Initialize Dcamapi
+        if Dcamapi.init() is not False:
+            n = Dcamapi.get_devicecount()
+            if verbose:
+                print("Found %d cameras" % n)
+
+            # Initialize the cameras
+            low_resolution_camera = Dcam(0)
+            high_resolution_camera = Dcam(1)
+
+            # Confirm cameras are open, and set default parameters
+            if low_resolution_camera.dev_open() is not False:
+                low_resolution_camera.dcam_set_default_light_sheet_mode_parameters()
+            else:
+                print('-NG: low_resolution_camera.dev_open() fails with error {}'.format(low_resolution_camera.lasterr()))
+                sys.exit()
+
+            if high_resolution_camera.dev_open() is not False:
+                high_resolution_camera.dcam_set_default_light_sheet_mode_parameters()
+            else:
+                print('-NG: high_resolution_camera.dev_open() fails with error {}'.format(high_resolution_camera.lasterr()))
+                sys.exit()
+
+            if verbose:
+                print("Cameras Initialized")
+
         else:
-            print('-NG: low_resolution_camera.dev_open() fails with error {}'.format(low_resolution_camera.lasterr()))
+            print('-NG: Dcamapi.init() fails with error {}'.format(Dcamapi.lasterr()))
             sys.exit()
-
-        if high_resolution_camera.dev_open() is not False:
-            high_resolution_camera.dcam_set_default_light_sheet_mode_parameters()
-        else:
-            print('-NG: high_resolution_camera.dev_open() fails with error {}'.format(high_resolution_camera.lasterr()))
-            sys.exit()
-
-        if verbose:
-            print("Cameras Initialized")
-
     else:
-        print('-NG: Dcamapi.init() fails with error {}'.format(Dcamapi.lasterr()))
-        sys.exit()
+        pass
 
     # Starts the GUI event loop and presents gui to user
     # Instance of the main window any additional windows will be toplevel classes

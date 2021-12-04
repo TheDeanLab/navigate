0eb824682f8923064c6e621ffed4fadc833088bb
Microscopy Innovation Lab <42547789+AdvancedImagingUTSW@users.noreply.github.com>
2021-12-04 12:03:22 -0600

    Adding camera settings frame tab to the GUI
    Very early in the process.

---
+++ base/1.0.0/view/tabs/camera_settings/camera_roi.py
@@ -0,0 +1,30 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter.font import Font
+import numpy as np
+
+class camera_roi_frame(ttk.Frame):
+    def __init__(self, cam_settings, *args, **kwargs):
+
+        #Init Frame
+        ttk.Frame.__init__(self, cam_settings, *args, **kwargs)
+
+        # Creating Dropdowns
+        cam_settings.laser_options = StringVar()
+        cam_settings.laser_pull_down = ttk.Combobox(cam_settings)
+        cam_settings.laser_pull_down['values'] = ['Normal Mode', 'Light-Sheet Mode']
+        cam_settings.laser_pull_down.current(0)
+        cam_settings.laser_pull_down.grid(row=0, column=1, sticky=(NSEW))
+        #TODO: Have it save the parameters to session.
+
+
+class camera_roi_label_frame(ttk.Frame):
+    def __init__(self, settings_tab, *args, **kwargs):
+
+        #Init Frame
+        ttk.Frame.__init__(self, settings_tab, *args, **kwargs)
+
+        #Adding Labels to frame
+        self.laser_label = ttk.Label(self, text='Camera ROI')
+        self.laser_label.grid(row=0, column=0, sticky=(NSEW))
+
--- base/1.0.0/view/tabs/camera_settings_tab.py
+++ base/1.0.0/view/tabs/camera_settings_tab.py
@@ -3,8 +3,19 @@ from tkinter import ttk
 from tkinter.font import Font
 
 import numpy as np
+from view.tabs.camera_settings.camera_roi import camera_roi_frame, camera_roi_label_frame
 
 class camera_settings_tab(ttk.Frame):
-    def __init__(cam_settings, setntbk, session, *args, **kwargs):
+    def __init__(self, setntbk, session, *args, **kwargs):
         #Init Frame
-        ttk.Frame.__init__(cam_settings, setntbk, *args, **kwargs)
+        ttk.Frame.__init__(self, setntbk, *args, **kwargs)
+
+        #Camera ROI Settings
+        self.camera_roi_label = ttk.Labelframe(self, text='Camera Settings')
+        self.camera_roi_label.grid(row=0, column=0, sticky=(NSEW))
+        self.camera_roi_label_frame = camera_roi_label_frame(self.camera_roi_label)
+        self.camera_roi_label_frame.grid(row=0, column=1, columnspan=3, sticky=(NSEW))
+
+        #Stack Cycling Settings
+        self.roi_frame = camera_roi_frame(self)
+        self.roi_frame.grid(row=1, column=0, columnspan=5, sticky=(NSEW), pady=10)

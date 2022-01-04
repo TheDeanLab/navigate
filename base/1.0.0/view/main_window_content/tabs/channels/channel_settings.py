3bb0bd14588e78dff59ce016b60ff0f5bae1d3d1
codeCollision4 <collisod@gmail.com>
2022-01-04 15:35:56 -0600

    New Loop/Array based widget classes for task #2
    Refactored channel settings to be column based instead of row based. Now easier to adjust. We had idea to be able to specify how many channels you want on startup with the yaml file. This number will be passed to the classes to be used in the widget construction.

--- base/1.0.0/view/main_window_content/tabs/channels/channel_settings.py
+++ base/1.0.0/view/main_window_content/tabs/channels/channel_settings.py
@@ -53,3 +53,106 @@ class channel_frame(ttk.Frame):
         channel.exp_time_spinbox = ttk.Spinbox(channel, from_=0, to=5000.0, textvariable=channel.exp_time_spinval,
             increment=25, width=9)
         channel.exp_time_spinbox.grid(row=0, column=3, sticky=(NSEW))
+
+class channel_checkboxes(ttk.Frame):
+    def __init__(self, channels_tab, num, *args, **kwargs):
+
+        #Init Frame
+        ttk.Frame.__init__(self, channels_tab, *args, **kwargs)
+
+        self.channel_variables = []
+        self.channel_checks = []
+        for num in range(5):
+            self.channel_variables[num] = BooleanVar()
+            self.channel_checks[num] = ttk.Checkbutton(self, text='CH' + (num+1), variable=self.channel_variables[num])
+            self.channel_checks[num].grid(row=num, column=0, sticky=(NSEW))
+
+class laser_drops(ttk.Frame):
+    def __init__(self, channels_tab, num, *args, **kwargs):
+
+        #Init Frame
+        ttk.Frame.__init__(self, channels_tab, *args, **kwargs)
+
+        #Laser Label
+        self.laser_label = ttk.Label(self, text='Laser')
+        self.laser_label.grid(row=0, column=0, sticky=(NSEW))
+
+        #Drops
+        self.laser_variables = []
+        self.laser_pulldowns = []
+        for num in range(5):
+            self.laser_variables[num] = StringVar()
+            self.laser_pulldowns[num] = ttk.Combobox(self, textvariable=self.laser_variables[num])
+            self.laser_pulldowns[num].grid(row=num, column=0, sticky=(NSEW))
+
+class laserpower(ttk.Frame):
+    def __init__(self, channels_tab, num, *args, **kwargs):
+
+        #Init Frame
+        ttk.Frame.__init__(self, channels_tab, *args, **kwargs)
+
+        #Laser Label
+        self.laserpower_label = ttk.Label(self, text='Laser Power')
+        self.laserpower_label.grid(row=0, column=0, sticky=(NSEW))
+
+        #Drops
+        self.laserpower_variables = []
+        self.laserpower_pulldowns = []
+        for num in range(5):
+            self.laserpower_variables[num] = StringVar()
+            self.laserpower_pulldowns[num] = ttk.Combobox(self, textvariable=self.laserpower_variables[num])
+            self.laserpower_pulldowns[num].grid(row=num, column=0, sticky=(NSEW))
+
+class filterwheel_drops(ttk.Frame):
+    def __init__(self, channels_tab, num, *args, **kwargs):
+
+        #Init Frame
+        ttk.Frame.__init__(self, channels_tab, *args, **kwargs)
+
+        #Filterwheel Label
+        self.filterwheel_label = ttk.Label(self, text='Filterwheel')
+        self.filterwheel_label.grid(row=0, column=0, sticky=(NSEW))
+
+        #Drops
+        self.filterwheel_variables = []
+        self.filterwheel_pulldowns = []
+        for num in range(5):
+            self.filterwheel_variables[num] = StringVar()
+            self.filterwheel_pulldowns[num] = ttk.Combobox(self, textvariable=self.filterwheel_variables[num])
+            self.filterwheel_pulldowns[num].grid(row=num, column=0, sticky=(NSEW))
+
+class exposuretime_drops(ttk.Frame):
+    def __init__(self, channels_tab, num, *args, **kwargs):
+
+        #Init Frame
+        ttk.Frame.__init__(self, channels_tab, *args, **kwargs)
+
+        #Exposure Time Label
+        self.exptime_label = ttk.Label(self, text='Exposure Time (ms)')
+        self.exptime_label.grid(row=0, column=0, sticky=(NSEW))
+
+        #Drops
+        self.exptime_variables = []
+        self.exptime_pulldowns = []
+        for num in range(5):
+            self.exptime_variables[num] = StringVar()
+            self.exptime_pulldowns[num] = ttk.Combobox(self, textvariable=self.exptime_variables[num])
+            self.exptime_pulldowns[num].grid(row=num, column=0, sticky=(NSEW))
+
+class interval_spins(ttk.Frame):
+    def __init__(self, channels_tab, num, *args, **kwargs):
+
+        #Init Frame
+        ttk.Frame.__init__(self, channels_tab, *args, **kwargs)
+
+        #Time Interval Label
+        self.interval_label = ttk.Label(self, text='Time Interval (ms)')
+        self.interval_label.grid(row=0, column=0, sticky=(NSEW))
+
+        #Spins
+        self.interval_variables = []
+        self.interval_pulldowns = []
+        for num in range(5):
+            self.interval_variables[num] = StringVar()
+            self.interval_pulldowns[num] = ttk.Combobox(self, textvariable=self.interval_variables[num])
+            self.interval_pulldowns[num].grid(row=num, column=0, sticky=(NSEW))

be0c4b59417b690a52084ce142012b8ec0bf9aaf
codeCollision4 <collisod@gmail.com>
2022-01-28 17:16:27 -0600

    Starting Custom Popup #37
    Had some existing code, creating an example for the acquire popup. Idea is to use the popup class and call it like you would a combobox widget. Comments try to explain. Then in the acquire popup example you just create the popup widget and add widgets to that in the class to remove the repetitive code of creating the popup window

--- src/view/custom_widgets/popup.py
+++ src/view/custom_widgets/popup.py
@@ -1,14 +1,32 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk
-from view.notebooks.acquire_bar_frame.popup_entries import popup_entries
 
-##TODO goal of this class is to create a generic popup that can be used for any purpose
+
+## goal of this class is to create a generic popup that can be used for any purpose
 
 #Class that handles the dialog box that has all the user entry stuff when you press the Acquisition button
 class PopUp():
 
     def __init__(self, root, name, size, *args, **kwargs):
+        '''
+        #### Creates the popup window based on the root window being passed, title that you want the window to have and the size of the window.
+        Some important things to consider:
+
+        - Root has to be the main application window to work
+
+        - Name has to be a string
+
+        - Size also has to be a string in the format '600x400+320+180'
+
+        - 600x400 represents the pixel size
+        
+        - +320 means 320 pixels from left edge, +180 means 180 pixels from top edge.
+
+        - If a '-' is used insetead of '+' it will be from the opposite edge.
+
+        - The parent frame for any widgets you add to the popup will be retrieved with the get_frame() function
+        '''
         #This starts the popup window config, and makes sure that any child widgets can be resized with the window
         self.popup = tk.Toplevel()
         self.popup.title(name)
@@ -20,16 +38,18 @@ class PopUp():
         
         self.popup.protocol("WM_DELETE_WINDOW", self.dismiss) #Intercepting close button
         self.popup.transient(root) #Prevents clicking outside of window
+
+        # The two below functions are examples of what we can use during testing
         self.popup.wait_visibility() # Can't grab until window appears, so we wait
         self.popup.grab_set()   #Ensures any input goes to this window
 
         #Putting popup frame into toplevel window
-        self.popup_frame = ttk.Frame(self.popup)
-        self.popup_frame.grid(row=0, column=0, sticky=(NSEW))
+        self.content_frame = ttk.Frame(self.popup)
+        self.content_frame.grid(row=0, column=0, sticky=(NSEW))
 
-        #Creating content to put into popup frame
-        self.content = popup_entries(self.popup_frame, self)
-        self.content.grid(row=0, column=0, sticky=(NSEW))
+        # #Creating content to put into popup frame
+        # self.content = popup_widgets(self.popup_frame, self)
+        # self.content.grid(row=0, column=0, sticky=(NSEW))
         
     #Catching close buttons/destroying window procedures
         #Dismiss function for destroying window when done
@@ -37,3 +57,7 @@ class PopUp():
     def dismiss(self, verbose=False):
         self.popup.grab_release() #Ensures input can be anywhere now
         self.popup.destroy()
+    
+    #Function so that popup entries can have a parent frame
+    def get_frame(self):
+        return self.content_frame

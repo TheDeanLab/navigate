ed0a8fc8cf18897b9bae9535bc0d8f214b7ca502
Andrew Jamieson <andrew.jamieson.utsw@gmail.com>
2022-02-01 14:48:28 -0600

    format correction

--- src/view/main_application_window.py
+++ src/view/main_application_window.py
@@ -1,4 +1,4 @@
-#Think of tkinter as HTML, ttk as CSS, and Python as javascript
+# Think of tkinter as HTML, ttk as CSS, and Python as javascript
 '''
 A main window is created and passed to the mainapp class. This class will init as a frame then config the main window. It then 
 creates a menubar using the menubar class. Adds the options for each file menu. It then sets up the frames, then grids the frames.
@@ -22,48 +22,49 @@ from .main_window_content.stagecontrol_notebook import stagecontrol_maxintensity
 from .main_window_content.acquire_bar_frame.acquire_bar import AcquireBar
 from .main_window_content.menus import menubar
 
-#Creates the frame that will hold the GUI content, its parent is the main window or root Tk object
+
+# Creates the frame that will hold the GUI content, its parent is the main window or root Tk object
 class Main_App(ttk.Frame):
-        #Takes a Tk object should be something like root = Tk() then root_window(root)
+    # Takes a Tk object should be something like root = Tk() then root_window(root)
 
-        def __init__(mainapp, root, *args, **kwargs):
-                #Inits this class as a frame subclass with the root as its parent
-                ttk.Frame.__init__(mainapp, root, *args, **kwargs)
+    def __init__(mainapp, root, *args, **kwargs):
+        # Inits this class as a frame subclass with the root as its parent
+        ttk.Frame.__init__(mainapp, root, *args, **kwargs)
 
-                #This starts the main window config, and makes sure that any child widgets can be resized with the window
-                mainapp.root = root
-                mainapp.root.title("Multiscale Axially Swept Light-Sheet Microscope")
-                view_directory = Path(__file__).resolve().parent  # keep icons relative to view directory structure
-                photo_image = view_directory.joinpath("icon", "mic.png")
-                mainapp.root.iconphoto(True, PhotoImage(file=photo_image))
-                mainapp.root.minsize(1400, 700)
-                mainapp.root.columnconfigure(0,weight=1)
-                mainapp.root.rowconfigure(0,weight=1)
+        # This starts the main window config, and makes sure that any child widgets can be resized with the window
+        mainapp.root = root
+        mainapp.root.title("Multiscale Axially Swept Light-Sheet Microscope")
+        view_directory = Path(__file__).resolve().parent  # keep icons relative to view directory structure
+        photo_image = view_directory.joinpath("icon", "mic.png")
+        mainapp.root.iconphoto(True, PhotoImage(file=photo_image))
+        mainapp.root.minsize(1400, 700)
+        mainapp.root.columnconfigure(0, weight=1)
+        mainapp.root.rowconfigure(0, weight=1)
 
-                #Creating and linking menu to main window/app
-                mainapp.menubar = menubar(root)
+        # Creating and linking menu to main window/app
+        mainapp.menubar = menubar(root)
 
-                #Top Frame Acquire Bar
-                mainapp.top_frame = ttk.Frame(mainapp)
-                #mainapp.top_frame_label = ttk.Label(mainapp.top_frame, text="Acquire Bar")
-                #mainapp.top_frame_label.grid(row=0,column=0)
+        # Top Frame Acquire Bar
+        mainapp.top_frame = ttk.Frame(mainapp)
+        # mainapp.top_frame_label = ttk.Label(mainapp.top_frame, text="Acquire Bar")
+        # mainapp.top_frame_label.grid(row=0,column=0)
 
-                #Left Frame Notebook 1 setup
-                mainapp.frame_left = ttk.Frame(mainapp)
-                #mainapp.left_frame_label = ttk.Label(mainapp.frame_left, text="Notebook #1")
-                #mainapp.left_frame_label.grid(row=0,column=0)
+        # Left Frame Notebook 1 setup
+        mainapp.frame_left = ttk.Frame(mainapp)
+        # mainapp.left_frame_label = ttk.Label(mainapp.frame_left, text="Notebook #1")
+        # mainapp.left_frame_label.grid(row=0,column=0)
 
-                #Top right Frame Notebook 2 setup
-                mainapp.frame_top_right = ttk.Frame(mainapp)
-                #mainapp.top_right_frame_label = ttk.Label(mainapp.frame_top_right, text="Notebook #2")
-                #mainapp.top_right_frame_label.grid(row=0,column=0)
+        # Top right Frame Notebook 2 setup
+        mainapp.frame_top_right = ttk.Frame(mainapp)
+        # mainapp.top_right_frame_label = ttk.Label(mainapp.frame_top_right, text="Notebook #2")
+        # mainapp.top_right_frame_label.grid(row=0,column=0)
 
-                #Bottom right Frame Notebook 3 setup
-                mainapp.frame_bottom_right = ttk.Frame(mainapp)
-                #mainapp.bottom_right_frame_label = ttk.Label(mainapp.frame_bottom_right, text="Notebook #3")
-                #mainapp.bottom_right_frame_label.grid(row=0,column=0)
+        # Bottom right Frame Notebook 3 setup
+        mainapp.frame_bottom_right = ttk.Frame(mainapp)
+        # mainapp.bottom_right_frame_label = ttk.Label(mainapp.frame_bottom_right, text="Notebook #3")
+        # mainapp.bottom_right_frame_label.grid(row=0,column=0)
 
-                '''
+        '''
                 Placing the notebooks using grid. While the grid is called on each frame it is actually calling 
                 the main window since those are the parent to the frames. The labels have already been packed into each respective
                 frame so can be ignored in the grid setup. This layout uses a 2x2 grid to start. 
@@ -76,28 +77,30 @@ class Main_App(ttk.Frame):
                 spot 4 and bottom right frame takes spot 6. Top frame will be spots 1 & 2
                 '''
 
-                #Gridding out foundational frames
-                mainapp.grid(column=0, row=0, sticky=(NSEW)) #Sticky tells which walls of gridded cell the widget should stick to, in this case its sticking to the main window on all sides
-                mainapp.top_frame.grid(row=0, column=0, columnspan=2, sticky=(NSEW))
-                mainapp.frame_left.grid(row=1, column=0, rowspan=2, sticky=(NSEW))
-                mainapp.frame_top_right.grid(row=1, column=1, sticky=(NSEW))
-                mainapp.frame_bottom_right.grid(row=2, column=1, sticky=(NSEW))
+        # Gridding out foundational frames
+        mainapp.grid(column=0, row=0, sticky=(NSEW))  # Sticky tells which walls of gridded cell the widget should stick to, in this case its sticking to the main window on all sides
+        mainapp.top_frame.grid(row=0, column=0, columnspan=2, sticky=(NSEW))
+        mainapp.frame_left.grid(row=1, column=0, rowspan=2, sticky=(NSEW))
+        mainapp.frame_top_right.grid(row=1, column=1, sticky=(NSEW))
+        mainapp.frame_bottom_right.grid(row=2, column=1, sticky=(NSEW))
 
-                #This dictates how to weight each piece of the grid, so that when the window is resized the notebooks get the proper screen space. 
-                # mainapp or mainapp is the frame holding all the other frames that hold the notebooks to help modularize the code
-                mainapp.columnconfigure(0, weight=1) #can add an arg called min or max size to give starting point for each frame
-                mainapp.columnconfigure(1, weight=1) #weights are relative to each other so if there is a 3 and 1 the 3 weight will give that col/row 3 pixels for every one the others get
-                mainapp.rowconfigure(0, weight=1)
-                mainapp.rowconfigure(1,weight=1)
+        # This dictates how to weight each piece of the grid, so that when the window is resized the notebooks get the proper screen space.
+        # mainapp or mainapp is the frame holding all the other frames that hold the notebooks to help modularize the code
+        mainapp.columnconfigure(0,
+                                weight=1)  # can add an arg called min or max size to give starting point for each frame
+        mainapp.columnconfigure(1,
+                                weight=1)  # weights are relative to each other so if there is a 3 and 1 the 3 weight will give that col/row 3 pixels for every one the others get
+        mainapp.rowconfigure(0, weight=1)
+        mainapp.rowconfigure(1, weight=1)
 
-                #Putting Notebooks into frames, tabs are held within the class of each notebook
-                mainapp.notebook_1 = notebook_1(mainapp.frame_left)
-                mainapp.notebook_2 = notebook_2(mainapp.frame_top_right)
-                mainapp.notebook_3 = notebook_3(mainapp.frame_bottom_right)
-                mainapp.acqbar = AcquireBar(mainapp.top_frame, mainapp.root)
+        # Putting Notebooks into frames, tabs are held within the class of each notebook
+        mainapp.notebook_1 = notebook_1(mainapp.frame_left)
+        mainapp.notebook_2 = notebook_2(mainapp.frame_top_right)
+        mainapp.notebook_3 = notebook_3(mainapp.frame_bottom_right)
+        mainapp.acqbar = AcquireBar(mainapp.top_frame, mainapp.root)
 
 
 if __name__ == '__main__':
-        root = tk.Tk()
-        Main_App(root)
-        root.mainloop()
+    root = tk.Tk()
+    Main_App(root)
+    root.mainloop()

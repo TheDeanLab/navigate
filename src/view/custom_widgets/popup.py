ee5728c6a04d5f442e9ef50695f89ffba47ed803
codeCollision4 <79557266+codeCollision4@users.noreply.github.com>
2022-02-10 13:01:44 -0600

    Generic Popup Complete #37
    I finished setting up the generic popup class and replaced the code in the acquire_popup to now use it. I updated the acquire_bar_controller to use some of the data retrieval functions for the popup but I didn't change too much, if there are any bugs with the popup it will probably be there @annie-xd-wang

--- src/view/custom_widgets/popup.py
+++ src/view/custom_widgets/popup.py
@@ -2,13 +2,16 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 
+#https://stackoverflow.com/questions/28560209/transient-input-window
+# Above link is a resource for using popups. Some helpful tips of an easy way to access the data inputted by a user into the popup
+# Also discusses transience of a popup (whether you can click out of the popup)
 
 ## goal of this class is to create a generic popup that can be used for any purpose
 
 #Class that handles the dialog box that has all the user entry stuff when you press the Acquisition button
-class PopUp():
+class PopUp(tk.Toplevel):
 
-    def __init__(self, root, name, size, *args, **kwargs):
+    def __init__(self, root, name, size, transient=True, *args, **kwargs):
         '''
         #### Creates the popup window based on the root window being passed, title that you want the window to have and the size of the window.
         Some important things to consider:
@@ -25,26 +28,31 @@ class PopUp():
 
         - If a '-' is used insetead of '+' it will be from the opposite edge.
 
+        - Transient is a boolean
+
         - The parent frame for any widgets you add to the popup will be retrieved with the get_frame() function
         '''
+        tk.Toplevel.__init__(self)
         #This starts the popup window config, and makes sure that any child widgets can be resized with the window
-        self.popup = tk.Toplevel()
-        self.popup.title(name)
-        self.popup.geometry(size) #300x200 pixels, first +320 means 320 pixels from left edge, +180 means 180 pixels from top edge
-        self.popup.columnconfigure(0, weight=1)
-        self.popup.rowconfigure(0, weight=1)
-        self.popup.resizable(FALSE, FALSE) #Makes it so user cannot resize
-        self.popup.attributes("-topmost", 1) #Makes it be on top of mainapp when called
+        self.title(name)
+        self.geometry(size) #300x200 pixels, first +320 means 320 pixels from left edge, +180 means 180 pixels from top edge
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.resizable(FALSE, FALSE) #Makes it so user cannot resize
+        self.attributes("-topmost", 1) #Makes it be on top of mainapp when called
         
-        self.popup.protocol("WM_DELETE_WINDOW", self.dismiss) #Intercepting close button
-        self.popup.transient(root) #Prevents clicking outside of window
+        self.protocol("WM_DELETE_WINDOW", self.dismiss) #Intercepting close button
+
+        #Checks if you want transience
+        if transient == True:
+            self.transient(root) #Prevents clicking outside of window
 
         # The two below functions are examples of what we can use during testing
-        self.popup.wait_visibility() # Can't grab until window appears, so we wait
-        self.popup.grab_set()   #Ensures any input goes to this window
+        self.wait_visibility() # Can't grab until window appears, so we wait
+        self.grab_set()   #Ensures any input goes to this window
 
         #Putting popup frame into toplevel window
-        self.content_frame = ttk.Frame(self.popup)
+        self.content_frame = ttk.Frame(self)
         self.content_frame.grid(row=0, column=0, sticky=(NSEW))
 
         # #Creating content to put into popup frame
@@ -55,8 +63,11 @@ class PopUp():
         #Dismiss function for destroying window when done
 
     def dismiss(self, verbose=False):
-        self.popup.grab_release() #Ensures input can be anywhere now
-        self.popup.destroy()
+        '''
+        #### Releases control back to main window from popup
+        '''
+        self.grab_release() #Ensures input can be anywhere now
+        self.destroy()
     
     #Function so that popup entries can have a parent frame
     def get_frame(self):
--- src/view/main_window_content/acquire_bar_frame/acquire_bar.py
+++ src/view/main_window_content/acquire_bar_frame/acquire_bar.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-from .acquire_popup import Acquire_PopUp
+
 
 
 class AcquireBar(ttk.Frame):

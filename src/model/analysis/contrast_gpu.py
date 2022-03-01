1f070bd95e25a56375a344fc22b48ebb82b6a5a1
Microscopy Innovation Lab <42547789+AdvancedImagingUTSW@users.noreply.github.com>
2022-03-01 14:49:41 -0600

    Update contrast_gpu.py
    Trying to finish all of the operations with TensorFlow...  Getting a NaN for a entropy value.  Don't have time to look into it.  But the return on investment looks minimal.  When I switch back to a numpy array and then calculate the entropy there, it takes the same amount of time for me to get the NaN value incorrectly with tf.

--- src/model/analysis/contrast_gpu.py
+++ src/model/analysis/contrast_gpu.py
@@ -2,6 +2,7 @@ from tifffile import imread
 import numpy as np
 import tensorflow as tf
 import time
+import os
 
 
 def normalized_dct_shannon_entropy(input_array, PSF_support_diameter_xy=3, verbose=False):
@@ -11,6 +12,7 @@ def normalized_dct_shannon_entropy(input_array, PSF_support_diameter_xy=3, verbo
     # OTF_support_y : Support for the OTF in the y-dimension.
     # Returns the entropy value.
     '''
+    use_numpy = True
 
     # Image Attributes
     input_array = np.double(input_array)
@@ -41,31 +43,66 @@ def normalized_dct_shannon_entropy(input_array, PSF_support_diameter_xy=3, verbo
         tensor_array = tf.signal.dct(tf.transpose(tensor_array), type=2)
         tensor_array = tf.signal.dct(tensor_array, type=2)
 
-        # Normalize DCT
+        # Normalize DCT - dtype = 'float64'
         tensor_array = tf.math.divide(tensor_array, tf.norm(tensor_array, ord=2))
 
-        # Convert to Numpy Array
-        dct_array = tensor_array.numpy()
+        if use_numpy:
+            # Convert to Numpy Array, and Calculate Entropy
+            dct_array = tensor_array.numpy()
+            i = dct_array > 0
+            image_entropy = np.sum(dct_array[i] * np.log(dct_array[i]))
+            image_entropy = image_entropy + np.sum(-dct_array[~i] * np.log(-dct_array[~i]))
+            image_entropy = -2*image_entropy / (OTF_support_x * OTF_support_y)
 
-        # Calculate Entropy
-        i = dct_array > 0
-        image_entropy = np.sum(dct_array[i] * np.log(dct_array[i]))
-        image_entropy = image_entropy + np.sum(-dct_array[~i] * np.log(-dct_array[~i]))
-        image_entropy = -2*image_entropy / (OTF_support_x * OTF_support_y)
+        else:
+            # Use TensorFlow Math to Calculate Entropy
+            data_type = tf.float32
+            entropy_threshold = tf.convert_to_tensor([1], dtype=data_type)
+            tensor_array = tf.cast(tensor_array, dtype=data_type)
+            i = tf.math.greater(tensor_array, entropy_threshold)
+            image_entropy = tf.math.reduce_sum(tensor_array[i] * tf.math.log(tensor_array[i]))
+            image_entropy = tf.math.add(image_entropy,
+                                        tf.math.reduce_sum(-tensor_array[~i] * tf.math.log(-tensor_array[~i])))
 
-        if (verbose):
+            otf_constant = tf.constant([OTF_support_x * OTF_support_y], dtype=data_type)
+            image_entropy = tf.math.divide(image_entropy, otf_constant)
+            image_entropy = tf.multiply(tf.constant([-2], dtype=data_type), image_entropy)
+            image_entropy = image_entropy.numpy()
+
+        if verbose:
             print("DCTS Entropy:", image_entropy)
             print("Execution Time:", time.time() - start_time)
 
         entropy[image_idx] = image_entropy
     return entropy
 
+def initiate_gpu():
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        # Create 2 virtual GPUs with 24GB memory each
+        try:
+            tf.config.set_logical_device_configuration(gpus[0],
+                                                       [tf.config.LogicalDeviceConfiguration(memory_limit=23000),
+                                                        tf.config.LogicalDeviceConfiguration(memory_limit=23000)])
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPU,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Virtual devices must be set before GPUs have been initialized
+            print(e)
+
 if (__name__ == "__main__"):
+    initiate_gpu()
+
+    # Contrast metrics testing
     import os
 
     # Contrast metrics testing
-    image_data = imread(os.path.join("E:", "test_data", "CH01_003.tif"))
+    image_data = imread(os.path.join("E:", "test_data", "CH01_003b.tif"))
     PSF_support = 3
     verbose = True
 
     entropy = normalized_dct_shannon_entropy(image_data, PSF_support, verbose)
+
+    # entropy_threshold = tf.cast(entropy_threshold, dtype=tf.float64)
+    # entropy_threshold = np.array((0), dtype=np.float64)
+    # entropy_threshold = tf.constant([1], shape=tensor_array.shape, dtype=tf.float64)

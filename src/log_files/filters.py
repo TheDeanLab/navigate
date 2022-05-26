5d518462d792099405c0e571d5a1b8c4db9b8bbc
Dax Collison <collisod@gmail.com>
2022-05-26 18:28:13 -0500

    Model now has own performance log due to Process #53

--- src/log_files/filters.py
+++ src/log_files/filters.py
@@ -1,9 +1,11 @@
 import logging
 
+
 class PerformanceFilter(logging.Filter):
     def filter(self, record):
         # Checking if log message should be sent to performance.log based on if it starts with Performance or Spec
         return record.getMessage().startswith('Performance' or 'Spec')
+
 
 class NonPerfFilter(logging.Filter):
     def filter(self, record):

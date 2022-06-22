b4a50565c2ed74f2a136030edabb6fe14b1627f0
Dax Collison <collisod@gmail.com>
2022-06-22 10:47:01 -0500

    Update stage_control_tab.py

--- src/aslm/view/main_window_content/stage_control/stage_control_tab.py
+++ src/aslm/view/main_window_content/stage_control/stage_control_tab.py
@@ -33,7 +33,6 @@ POSSIBILITY OF SUCH DAMAGE.
 # Local Imports
 from view.main_window_content.stage_control.tabs.other_axis_frame import other_axis_frame
 from view.main_window_content.stage_control.tabs.position_frame import position_frame
-from view.main_window_content.stage_control.tabs.x_y_frame import x_y_frame
 from view.main_window_content.stage_control.tabs.goto_frame import goto_frame
 # Standard Imports
 from tkinter import *

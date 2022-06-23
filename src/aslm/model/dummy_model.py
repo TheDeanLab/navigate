4d5ae0fa92c1c8f0644887cbd67ba934ee7a3ee5
Kevin Dean <42547789+AdvancedImagingUTSW@users.noreply.github.com>
2022-06-23 14:06:04 -0500

    Redo dummy model math
    Co-Authored-By: Zach Marin <zacsimile@users.noreply.github.com>
    Co-Authored-By: Dax Collison <79557266+codeCollision4@users.noreply.github.com>

--- src/aslm/model/dummy_model.py
+++ src/aslm/model/dummy_model.py
@@ -7,8 +7,8 @@ def get_dummy_model():
     Creates a dummy model to be used for testing. All hardware is synthetic and the current config settings are loaded.
     '''
     # Set up the model, experiment, ETL dictionaries
-    base_directory = Path(__file__).resolve().parent.parent.parent.parent
-    configuration_directory = Path.joinpath(base_directory, 'src/aslm', 'config')
+    base_directory = Path(__file__).resolve().parent.parent
+    configuration_directory = Path.joinpath(base_directory, 'config')
 
     config = Path.joinpath(configuration_directory, 'configuration.yml')
     experiment = Path.joinpath(configuration_directory, 'experiment.yml')

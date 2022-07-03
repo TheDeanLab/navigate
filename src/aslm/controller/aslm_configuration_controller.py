d0a59613538349a5bfe8109cb9a69d50134c6828
Microscopy Innovation Lab <42547789+AdvancedImagingUTSW@users.noreply.github.com>
2022-07-03 14:13:00 -0500

    Configuration controller.

--- src/aslm/controller/aslm_configuration_controller.py
+++ src/aslm/controller/aslm_configuration_controller.py
@@ -1,7 +1,4 @@
-"""
-ASLM configuration controller.
-
-Copyright (c) 2021-2022  The University of Texas Southwestern Medical Center.
+"""Copyright (c) 2021-2022  The University of Texas Southwestern Medical Center.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -32,8 +29,14 @@ IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 """
+
+# Standard Library Imports
 import logging
-from pathlib import Path
+
+# Third Party Imports
+
+# Local Imports
+
 # Logger Setup
 p = __name__.split(".")[1]
 logger = logging.getLogger(p)
@@ -43,46 +46,65 @@ class ASLM_Configuration_Controller:
     def __init__(self, configuration):
         self.configuration = configuration
 
-    def get_channels_info(self, verbose=False):
-        """
-        # Populates the channel combobox with the channels that are available in the model.configuration
+    def get_channels_info(self):
+        r"""Populate the channel combobox with the channels that are available in the model.configuration
+
+        Returns
+        -------
+        setting : dict
+            Channel settings, e.g. {
+                'laser': ['488nm', '562nm', '642nm'],
+                'filter': ['Empty-Alignment', 'GFP - FF01-515/30-32', '...}
         """
 
-        setting = {'laser': self.get_lasers_info(verbose), 'filter': list(
+        setting = {'laser': self.get_lasers_info(), 'filter': list(
             self.configuration.FilterWheelParameters['available_filters'].keys()), }
         return setting
 
-    def get_lasers_info(self, verbose=False):
+    def get_lasers_info(self):
+        r"""Populate the laser combobox with the lasers that are available in the model.configuration
+
+        Returns
+        -------
+        laser_list : list
+            List of lasers, e.g. ['488nm', '562nm', '642nm']
         """
-        # Populates the laser combobox with the lasers that are available in the model.configuration
-        """
-        number_of_lasers = int(
-            self.configuration.LaserParameters['number_of_lasers'])
+        number_of_lasers = int(self.configuration.LaserParameters['number_of_lasers'])
         laser_list = []
         for i in range(number_of_lasers):
-            laser_wavelength = self.configuration.LaserParameters['laser_' + str(
-                i) + '_wavelength']
+            laser_wavelength = self.configuration.LaserParameters['laser_' + str(i) + '_wavelength']
             laser_list.append(laser_wavelength)
-        if verbose:
-            print('Laser list: ', laser_list)
         return laser_list
 
-    def get_pixels(self, verbose=False):
-        """
-        # Gets default pixel values from camera
+    def get_pixels(self):
+        r"""Get default pixel values from camera
+
+        Returns
+        -------
+        x_pixels : int
+            Number of x pixels
+        y_pixels : int
+            Number of y pixels
         """
         return [self.configuration.CameraParameters['x_pixels'],
                 self.configuration.CameraParameters['y_pixels']]
 
+<<<<<<< Updated upstream
     def get_framerate(self, verbose=False):
         '''
         # Gets default framerate info from camera
         '''
         pass  # TODO Kevin this is where you pull in and then calculate the info from the config file to initializing the framerate widgets
 
+=======
+>>>>>>> Stashed changes
     def get_stage_position(self):
-        """
-        # Returns the current position of the stage
+        r"""Get current position of the stage
+
+        Returns
+        -------
+        position : dict
+            Dictionary with x, y, z, theta, and f positions.
         """
         stage_position = self.configuration.StageParameters['position']
         position = {
@@ -95,8 +117,12 @@ class ASLM_Configuration_Controller:
         return position
 
     def get_stage_step(self):
-        """
-        # Returns the step size of the stage
+        r"""Get the step size of the stage
+
+        Returns
+        -------
+        steps : dict
+            Step size in x (same step size for y), z, theta, and f.
         """
         steps = {
             'x': self.configuration.StageParameters['xy_step'],
@@ -107,8 +133,18 @@ class ASLM_Configuration_Controller:
         return steps
 
     def get_stage_position_limits(self, suffix):
-        """
-        # Returns the position limits of the stage
+        r"""Return the position limits of the stage
+
+        Parameters
+        ----------
+        suffix : str
+            '_min' or '_max'
+
+        Returns
+        -------
+        position_limits : dict
+            Depending on suffix, min or max stage limits, e.g. {'x': 2000, 'y': 2000, 'z': 2000, 'theta': 0, 'f': 2000}.
+
         """
         axis = ['x', 'y', 'z', 'theta', 'f']
         position_limits = {}
@@ -117,12 +153,16 @@ class ASLM_Configuration_Controller:
         return position_limits
 
     def get_etl_info(self):
+        r"""Return delay_percent, pulse_percent.
+
+        Returns
+        -------
+        remote_focus_parameters : dict
+            Dictionary with the remote focus percent delay and pulse percent.
         """
-        # return delay_percent, pulse_percent
-        """
-        temp = {
+        remote_focus_parameters = {
             'remote_focus_l_delay_percent': self.configuration.LaserParameters['laser_l_delay_percent'],
             'remote_focus_l_pulse_percent': self.configuration.LaserParameters['laser_l_pulse_percent'],
             'remote_focus_r_delay_percent': self.configuration.LaserParameters['laser_r_delay_percent'],
             'remote_focus_r_pulse_percent': self.configuration.LaserParameters['laser_r_pulse_percent']}
-        return temp
+        return remote_focus_parameters

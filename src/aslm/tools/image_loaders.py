3e2d04160a72e5a35314dd5407b31d07713e4500
zacsimile <zachcm@gmail.com>
2022-07-06 21:19:08 -0500

    Convert to xyz slicing

--- src/aslm/tools/image_loaders.py
+++ src/aslm/tools/image_loaders.py
@@ -5,21 +5,10 @@ import glob
 class LazyTiff:
     def __init__(self, fp):
         """
-        Lazy-loads a folder containing a sequence of TIFF files as a 3D image stack.
-        Assumes all TIFF files are single slices, with the same x and y dims. Image is
-        slicable as if a numpy array, it may just take a minute to load all of the images
-        depending on how big a slice you want.
-
-        Usage is, e.g.:
-            low_res_fp = '/endosome/archive/MIL/marin/Zach/Lung/Alignment/AntiRFP/2022-07-02/Cell_001'
-            low_res_data = LazyTiff(low_res_fp)
-            imshow(low_res_data[50, 500:1500, 500:1500])
-
-        Parameters
-        ----------
-        fp : str
-            Path to folder containing sequence of TIFF files.
+        Lazy-loads a folder containing a sequence of TIFF files using PIL.
+        Assumes all TIFF files are single slices, with the same x and y dims.
         """
+            
         # How many images in this sequence?
         self._files = sorted(glob.glob(fp+'/*.tif'))
         self._n_files = len(self._files)
@@ -34,7 +23,7 @@ class LazyTiff:
 
     @property
     def shape(self):
-        return (self._n_files, self._width, self._height)
+        return (self._width, self._height, self._n_files)
     
     def __getitem__(self, keys):
         """Numpy slicing access."""
@@ -43,24 +32,31 @@ class LazyTiff:
             keys = (keys,)
         if len(keys) > 3:
             raise IndexError(f"Cannot access this index. Shape is {self.shape}.")
+            
+        print(keys, self._image_index)
         
+        image_index = self._image_index
         if len(keys) == 0:
             return self._image
-
-        if keys[0] != self._image_index:
+        elif len(keys) < 2:
+            # Need to grab all of the files
+            new_files = self._files
+            self._image_index = ()
+        elif keys[2] != self._image_index:
             # load a new image
-            new_files = self._files[keys[0]]
+            new_files = self._files[keys[2]]
             if not isinstance(new_files, list):
                 new_files = [new_files]
-            self._image_index = keys[0]  # TODO: This flips between integer and slice
-            self._image = np.zeros((len(new_files), self._width, self._height), dtype=np.uint16)
+            self._image_index = keys[2]
+        
+        if image_index != self._image_index:
+            self._image = np.zeros((self._width, self._height, len(new_files)), dtype=np.uint16)
             for i, file in enumerate(new_files):
                 with PIL.Image.open(file) as im:
-                    self._image[i,...] = im
+                    self._image[...,i] = im
                     
         if len(keys) == 1:
-            return self._image.squeeze()
-        if len(keys) == 2:
-            return self._image[:,keys[1]].squeeze()
+            return self._image[keys[0],...]
         
-        return self._image[:,keys[1],keys[2]].squeeze()
+        return self._image[keys[0],keys[1],:].squeeze()
+        

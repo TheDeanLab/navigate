59a7353a7e6c5634a726f6188539680e48e61921
Annie Wang <annie.xd.wang@gmail.com>
2022-09-13 04:57:46 -0700

    load configuration to shared dictionary

--- src/aslm/config/config.py
+++ src/aslm/config/config.py
@@ -1,7 +1,11 @@
 import os
+import sys
 import shutil
 import platform
 from pathlib import Path
+from multiprocessing import Manager
+
+import yaml
 
 def get_configuration_paths():
     # Establish a base directory in AppData/Local/.ASLM for Windows or ~/.ASLM for Mac and Linux
@@ -38,3 +42,42 @@ def get_configuration_paths():
         shutil.copyfile(copy_etl_constants_path, etl_constants_path)
     
     return configuration_path, experiment_path, etl_constants_path
+
+def load_configs(manager, **kwargs):
+    if kwargs == {}:
+        print("No files provided to load_yaml_config()")
+        sys.exit(1)
+
+    config_dict = manager.dict()
+    for config_name, file_path in kwargs.items():
+        file_path = Path(file_path)
+        assert file_path.exists(), 'Configuration File not found: {}'.format(file_path)
+        with open(file_path) as f:
+            try:
+                config_data = yaml.load(f, Loader=yaml.FullLoader)
+                build_nested_dict(manager, config_dict, config_name, config_data)
+            except yaml.YAMLError as yaml_error:
+                print(f"Configurator - Yaml Error: {yaml_error}")
+                sys.exit(1)
+    
+    # return combined dictionary
+    # return config_dict
+
+    # return independent dictionaries
+    return config_dict.values()
+
+def build_nested_dict(manager, parent_dict, key_name, dict_data):
+    if type(dict_data) != dict and type(dict_data) != list:
+        parent_dict[key_name] = dict_data
+        return
+    if type(dict_data) == dict:
+        d = manager.dict()
+        for k in dict_data:
+            build_nested_dict(manager, d, k, dict_data[k])
+    else:
+        d = manager.list()
+        for i, v in enumerate(dict_data):
+            d.append(None)
+            build_nested_dict(manager, d, i, v)
+    parent_dict[key_name] = d
+    

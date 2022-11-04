a2fccb1b5208f9c399d1d6b1c2ac26c9d48aec51
Zach Marin <zachcm@gmail.com>
2022-11-04 17:25:23 -0500

    Add remote focus hardware test

---
+++ test/model/devices/remote_focus/test_remote_focus_ni.py
@@ -0,0 +1,23 @@
+import pytest
+
+@pytest.mark.hardware
+def test_remote_focus_ni_functions():
+    import random
+
+    from aslm.model.devices.daq.daq_ni import NIDAQ
+    from aslm.model.devices.remote_focus.remote_focus_ni import RemoteFocusNI
+    from aslm.model.dummy import DummyModel
+
+    model = DummyModel()
+    daq = NIDAQ(model.configuration)
+    microscope_name = model.configuration['experiment']['MicroscopeState']['microscope_name']
+    rf = RemoteFocusNI(microscope_name, daq, model.configuration)
+
+    funcs = ['initialize_task', 'adjust', 'prepare_task', 'start_task', 'stop_task', 'close_task']
+    args = [None, [random.random()], ['channel_dummy'], None, None, None]
+
+    for f, a in zip(funcs, args):
+        if a is not None:
+            getattr(rf, f)(*a)
+        else:
+            getattr(rf, f)()

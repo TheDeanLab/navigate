ddf7c122eca877105abc61d684de27d626f45d1d
Annie Wang <annie.xd.wang@gmail.com>
2022-11-09 17:03:36 -0800

    close camera after test

--- test/model/devices/APIs/hamamatsu/test_hamamatsu_api.py
+++ test/model/devices/APIs/hamamatsu/test_hamamatsu_api.py
@@ -1,5 +1,35 @@
-import random
-import time
+"""Copyright (c) 2021-2022  The University of Texas Southwestern Medical Center.
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted for academic and research use only (subject to the limitations in the disclaimer below)
+# provided that the following conditions are met:
+
+#      * Redistributions of source code must retain the above copyright notice,
+#      this list of conditions and the following disclaimer.
+
+#      * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+
+#      * Neither the name of the copyright holders nor the names of its
+#      contributors may be used to endorse or promote products derived from this
+#      software without specific prior written permission.
+
+# NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY
+# THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# """
+
 import pytest
 
 @pytest.mark.hardware
@@ -18,8 +48,12 @@ class TestHamamatsuAPI:
             except:
                 continue
 
+    def close_camera(self):
+        if self.camera != None:
+            self.camera.dev_close()
+
     def test_open_and_close_camera(self):
-        from aslm.model.devices.APIs.hamamatsu.HamamatsuAPI import DCAM, camReg
+        from aslm.model.devices.APIs.hamamatsu.HamamatsuAPI import camReg
         # open camera
         self.open_camera()
         if camReg.maxCameras > 0:
@@ -31,7 +65,6 @@ class TestHamamatsuAPI:
 
 
     def test_get_and_set_property_value(self):
-        from aslm.model.devices.APIs.hamamatsu.HamamatsuAPI import property_dict
         self.open_camera()
 
         # set property
@@ -64,7 +97,10 @@ class TestHamamatsuAPI:
         #set a non-exist property
         assert self.camera.set_property_value('non-exist-property', 100) == False, "can't handle non-exist property name"
 
+        self.close_camera()
+
     def test_ROI(self):
+        import random
         self.open_camera()
 
         rects = [(0, 0, 2047, 2047), (512, 512, 1535, 1535), (768, 768, 1279, 1279)]
@@ -75,9 +111,15 @@ class TestHamamatsuAPI:
             self.camera.set_ROI(*rect)
             assert self.camera.get_property_value('image_width') == (rect[2]-rect[0]+1), f'ROI Width: {(rect[2]-rect[0]+1)}'
             assert self.camera.get_property_value('image_height') == (rect[3]-rect[1]+1), f'ROI Height: {(rect[3]-rect[1]+1)}'
-        
 
+        self.close_camera()
+        
+    
     def test_acquisition(self):
+        import random
+        import time
+        from aslm.model.concurrency.concurrency_tools import SharedNDArray
+
         self.open_camera()
 
         configuration = {
@@ -96,7 +138,6 @@ class TestHamamatsuAPI:
         for k in configuration:
             self.camera.set_property_value(k, configuration[k])
 
-        from aslm.model.concurrency.concurrency_tools import SharedNDArray
 
         number_of_frames = 100
 
@@ -116,3 +157,5 @@ class TestHamamatsuAPI:
 
         self.camera.stop_acquisition()
         assert self.camera.is_acquiring == False, 'camera should stop acquiring data!'
+
+        self.close_camera()

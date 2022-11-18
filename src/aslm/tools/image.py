765e19c224ff7cbf628a4ee9cac79e6f4df6d503
Microscopy Innovation Lab <42547789+AdvancedImagingUTSW@users.noreply.github.com>
2022-11-18 15:23:45 -0600

    Token test contribution
    Simply killing time during study section...  We aren't even currently using the code that is now being tested, but it will artificially boost our codecov!

--- src/aslm/tools/image.py
+++ src/aslm/tools/image.py
@@ -1,9 +1,16 @@
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
+import platform
 
 
-def text_array(text: str, offset: tuple = (0, 0), font_size: int = 16):
+def text_array(text: str,
+               offset: tuple = (0, 0),
+               font_size: int = 16):
     """Create a binary array from a piece of text
+
+    Original font type was arial.ttf.
+    Not by default available on Darwin-based devices.
+    Switched to cambriab.ttf
 
     Parameters
     ----------
@@ -14,7 +21,7 @@ def text_array(text: str, offset: tuple = (0, 0), font_size: int = 16):
     font_size: int
         Size of font in pixels
     """
-    font = ImageFont.truetype("arial.ttf", font_size)
+    font = ImageFont.truetype("cambriab.ttf", font_size)
     bbox = font.getbbox(text)
     im = Image.new("1", (bbox[2] + offset[0], bbox[3] + offset[1]))
     ImageDraw.Draw(im).text(offset, text, 1, font=font)

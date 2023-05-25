b4aa2a4ec4e4ce5ce9428f070173aa1211ca65ff
Annie Wang <annie.xd.wang@gmail.com>
2023-05-25 15:52:02 -0700

    Update test_stage_pi.py

--- test/model/devices/stages/test_stage_pi.py
+++ test/model/devices/stages/test_stage_pi.py
@@ -54,7 +54,6 @@ class TestStagePI(unittest.TestCase):
         assert hasattr(stage, "z_pos")
         assert hasattr(stage, "f_pos")
         assert hasattr(stage, "theta_pos")
-        assert hasattr(stage, "position_dict")
         assert hasattr(stage, "x_max")
         assert hasattr(stage, "y_max")
         assert hasattr(stage, "z_max")
@@ -66,8 +65,8 @@ class TestStagePI(unittest.TestCase):
         assert hasattr(stage, "theta_min")
 
         # Methods
-        assert hasattr(stage, "create_position_dict") and callable(
-            getattr(stage, "create_position_dict")
+        assert hasattr(stage, "get_position_dict") and callable(
+            getattr(stage, "get_position_dict")
         )
         assert hasattr(stage, "report_position") and callable(
             getattr(stage, "report_position")

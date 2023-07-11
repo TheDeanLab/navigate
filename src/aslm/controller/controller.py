06de0f3d52df62ff6965cabb03b00d6741993a15
Microscopy Innovation Lab <42547789+AdvancedImagingUTSW@users.noreply.github.com>
2023-07-11 10:19:08 -0500

    Effectively revert #502 to fix autofocusing
    Co-Authored-By: Zach Marin <zacsimile@users.noreply.github.com>

--- src/aslm/controller/controller.py
+++ src/aslm/controller/controller.py
@@ -676,14 +676,14 @@ class Controller:
 
         elif command == "stop_acquire":
             """Stop the acquisition."""
-            self.stop_acquisition_flag = True
+            self.stop_acquisition_flag = False
 
             # self.model.run_command('stop')
             self.sloppy_stop()
 
             # clear show_img_pipe
-            while self.show_img_pipe.poll():
-                image_id = self.show_img_pipe.recv()
+            # while self.show_img_pipe.poll():
+            #     image_id = self.show_img_pipe.recv()
 
         elif command == "exit":
             """Exit the program."""
@@ -899,11 +899,11 @@ class Controller:
                 )
 
             # clear show_img_pipe
-            show_img_pipe = self.additional_microscopes[microscope_name]["show_img_pipe"]
-            while show_img_pipe.poll():
-                image_id = show_img_pipe.recv()
-                if image_id == "stop":
-                    break
+            # show_img_pipe = self.additional_microscopes[microscope_name]["show_img_pipe"]
+            # while show_img_pipe.poll():
+            #     image_id = show_img_pipe.recv()
+            #     if image_id == "stop":
+            #         break
 
             # start thread
             capture_img_thread = threading.Thread(

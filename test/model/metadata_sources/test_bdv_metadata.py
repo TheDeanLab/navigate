9df19e085fae3aa4206f7aaa3d5fcfe9c27ca51e
zacsimile <zachcm@gmail.com>
2023-08-21 21:43:45 -0500

    Test affine matrix organization

--- test/model/metadata_sources/test_bdv_metadata.py
+++ test/model/metadata_sources/test_bdv_metadata.py
@@ -1,7 +1,8 @@
 import os
+import numpy as np
 
 
-def test_bdv_metadata(dummy_model):
+def test_bdv_metadata():
     from aslm.model.metadata_sources.bdv_metadata import BigDataViewerMetadata
 
     md = BigDataViewerMetadata()
@@ -10,13 +11,19 @@ def test_bdv_metadata(dummy_model):
     for idx in range(10):
         views.append(
             {
-                "x": dummy_model.data_buffer_positions[idx][0],
-                "y": dummy_model.data_buffer_positions[idx][1],
-                "z": dummy_model.data_buffer_positions[idx][2],
-                "theta": dummy_model.data_buffer_positions[idx][3],
-                "f": dummy_model.data_buffer_positions[idx][4],
+                "x": np.random.randint(-1000, 1000),
+                "y": np.random.randint(-1000, 1000),
+                "z": np.random.randint(-1000, 1000),
+                "theta": np.random.randint(-1000, 1000),
+                "f": np.random.randint(-1000, 1000),
             }
         )
+
+    for view in views:
+        arr = md.stage_positions_to_affine_matrix(**view)
+        assert arr[0, 3] == view["y"] / md.dy
+        assert arr[1, 3] == view["x"] / md.dx
+        assert arr[2, 3] == view["z"] / md.dz
 
     md.write_xml("test_bdv.xml", views)
 

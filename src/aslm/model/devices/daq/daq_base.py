8146f12314cb96f5e5a4cbde332f27c9550b1388
Kevin Dean <42547789+AdvancedImagingUTSW@users.noreply.github.com>
2023-11-17 09:12:54 -0600

    Update daq_base.py
    Per @zacsimile's recommendation.

--- src/aslm/model/devices/daq/daq_base.py
+++ src/aslm/model/devices/daq/daq_base.py
@@ -73,10 +73,10 @@ class DAQBase:
 
         # Initialize Variables
         #: float: Sample rate of the DAQ
-        self.sample_rate = self.daq_parameters["sample_rate"]
+        self.sample_rate = None
 
         #: float: Sweep time of the DAQ
-        self.sweep_time = self.daq_parameters["sweep_time"]
+        self.sweep_time = None
 
         # Remote Focus Parameters
         #: dict: Dictionary of remote focus ramp falling percentages

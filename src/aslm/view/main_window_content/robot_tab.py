2af70c14eb442cd4d0f1d1a0abeb73965e4026fc
Abhijeet Kulkarni <137450031+abhikulkarni2002@users.noreply.github.com>
2023-11-17 21:32:20 -0600

    added pause and play buttons

--- src/aslm/view/main_window_content/robot_tab.py
+++ src/aslm/view/main_window_content/robot_tab.py
@@ -34,6 +34,7 @@
 import tkinter as tk
 from tkinter import ttk
 import logging
+from pathlib import Path
 
 # Third Party Imports
 
@@ -76,16 +77,22 @@ class RobotControlTab(tk.Frame):
         tk.Grid.columnconfigure(self, "all", weight=1)
         tk.Grid.rowconfigure(self, "all", weight=1)
 
-         # Robot Initialization Buttons
+        # Robot Initialization Buttons
         self.robot_init = RobotInitialization(self)
         self.robot_init.grid(
             row=0, column=0, columnspan=2, sticky=tk.NSEW, padx=10, pady=10
         )
 
         # Quick Command Buttons
-        self.quick_command = QuickCommands(self)
-        self.quick_command.grid(
+        self.move_sequence = MoveSequence(self)
+        self.move_sequence.grid(
             row=5, column=0, columnspan=2, sticky=tk.NSEW, padx=10, pady=10
+        )
+
+        # Pause and Play Buttons
+        self.pause_play = PausePlay(self)
+        self.pause_play.grid(
+            row=0, column=3, columnspan=2, sticky=tk.NSEW, padx=10, pady=10
         )
 
 class RobotInitialization(tk.Frame):
@@ -122,10 +129,10 @@ class RobotInitialization(tk.Frame):
 
         # Initializing Button
         self.buttons = {
-            "import": ttk.Button(self, text="Import"),
-            "connect": ttk.Button(self, text="Connect"),
-            "export": ttk.Button(self, text="Export"),
-            #add pause and play button
+            "import": ttk.Button(self, text="Load Positions from Disk"),
+            "connect": ttk.Button(self, text="Connect Robot"),
+            "export": ttk.Button(self, text="Save Positions to Disk "),
+            "launch": ttk.Button(self,text='Launch Robot Jog Wizard')
         }
         counter = 0
         for key, button in self.buttons.items():
@@ -134,18 +141,21 @@ class RobotInitialization(tk.Frame):
             elif counter == 1:
                 row, column = 0, 1
             elif counter == 2:
-                row, column = 0, 2
+                row, column = 1, 0
+            elif counter == 3:
+                row, column = 1,1
 
             button.grid(
                 row=row, column=column, sticky=tk.NSEW, padx=(4, 1), pady=(4, 6)
             )
             counter += 1
 
-class QuickCommands(tk.Frame):
-#change name to something more appropriate
-    """QuickCommands
 
-    QuickCommands is a frame that contains the widgets for initializing
+class MoveSequence(tk.Frame):
+#change name to something more appropriate
+    """MoveSequence
+
+    MoveSequence is a frame that contains the widgets for initializing
     robot movement.
 
     Parameters
@@ -194,3 +204,64 @@ class QuickCommands(tk.Frame):
                 row=row, column=column, sticky=tk.NSEW, padx=(4, 1), pady=(4, 6)
             )
             counter += 1
+
+class PausePlay(tk.Frame):
+    """PausePlay
+
+    PausePlay is a frame that contains the widgets for pausing and resuming
+    robot actions.
+
+    Parameters
+    ----------
+    settings_tab : tk.Frame
+        The frame that contains the settings tab.
+    *args : tuple
+        Variable length argument list.
+    **kwargs : dict
+        Arbitrary keyword arguments.
+
+    Attributes
+    ----------
+    buttons : dict
+        A dictionary of all the buttons that are tied to each widget name.
+        The key is the widget name, value is the button associated.
+
+    Methods
+    -------
+    """
+    def __init__(pauseplay_frame, settings_tab, *args, **kwargs):
+        ttk.Labelframe.__init__(pauseplay_frame, settings_tab, *args, **kwargs)
+
+        # Formatting
+        tk.Grid.columnconfigure(pauseplay_frame, "all", weight=1)
+        tk.Grid.rowconfigure(pauseplay_frame, "all", weight=1)
+
+        #Path to pause and play buttons
+        image_directory = Path(__file__).resolve().parent
+        pauseplay_frame.pause_image = tk.PhotoImage(
+            file=image_directory.joinpath("images", "pause.png")
+        ).subsample(32,32)
+        pauseplay_frame.play_image = tk.PhotoImage(
+            file=image_directory.joinpath("images", "play.png")
+        ).subsample(32,32)
+
+        # Pause Button
+        pauseplay_frame.pause_btn = tk.Button(
+            pauseplay_frame, 
+            image=pauseplay_frame.pause_image,
+            borderwidth=0,
+        )
+        # Play Button  
+        pauseplay_frame.play_btn = tk.Button(
+            pauseplay_frame,
+            image=pauseplay_frame.play_image,
+            borderwidth=0,
+        )
+
+        # Gridding out Buttons
+        pauseplay_frame.play_btn.grid(
+            row=0, column=0, rowspan=1, columnspan=1, padx=2, pady=2
+        )
+        pauseplay_frame.pause_btn.grid(
+            row=0, column=2, rowspan=1, columnspan=1, padx=2, pady=2
+        )

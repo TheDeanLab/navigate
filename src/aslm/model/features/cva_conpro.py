da96b253142f709f14fc3e62015ca075abccaaae
JinlongL <jinlong.lin@utsouthwestern.edu>
2023-11-20 18:10:26 -0600

    changed channel number acquisition and streamlined step size nm to mm conversion.

--- src/aslm/model/features/cva_conpro.py
+++ src/aslm/model/features/cva_conpro.py
@@ -35,7 +35,6 @@ import logging
 
 # Third Party Imports
 import numpy as np
-import re
 from aslm.model.features.image_writer import ImageWriter
 
 # from aslm.model import data_sources
@@ -125,10 +124,9 @@ class ConstantVelocityAcquisition:
         print("sweep times and exposure times calculated")
         print(f"exposure time = {exposure_times}")
         print(f"sweep time = {sweep_times}")
+
         channel_name = next(iter(sweep_times))
-        channel_name = str(channel_name)
-        channel_num = re.findall(r"[\d.]+", channel_name)
-        channel_num = int(channel_num[0])
+        channel_num = int(channel_name.split("_")[1])
         self.model.active_microscope.current_channel = channel_num
         print(f"channel_{self.model.active_microscope.current_channel}")
         current_sweep_time = sweep_times[
@@ -188,10 +186,9 @@ class ConstantVelocityAcquisition:
         print("Actual Step Size of Stage:", step_size_nm)
 
         # Calculate the actual step size in millimeters. 264 * 10^-6 mm
-        step_size_mm = step_size_nm / 1 * 10**-6  # 264 * 10^-6 mm
-        # TODO set max speed in configuration file
-        # max_speed = 4.288497*2
-
+        step_size_mm = step_size_nm * 10**-6  # 264 * 10^-6 mm
+        print(f"step size nm = {step_size_nm}")
+        print(f"step size mm = {step_size_mm}")
         # Set the start and end position of the scan in millimeters.
         # Retrieved from the GUI.
         # Set Stage Limits - Units in millimeters
@@ -261,7 +258,6 @@ class ConstantVelocityAcquisition:
 
         actual_mechanical_step_size_mm = stage_velocity * self.current_sweep_time
         self.actual_mechanical_step_size_um = actual_mechanical_step_size_mm * 1000
-        # self.desired_mechanical_step_size_mm = desired_mechanical_step_size_mm
         new_z_steps = np.floor(
             abs(self.stop_position_um - self.start_position_um)
             / self.actual_mechanical_step_size_um
@@ -297,16 +293,14 @@ class ConstantVelocityAcquisition:
         ]["expand"]
         print(f"repeat num = {self.repeat_waveform}")
         print(f"expand num = {self.expand_waveform}")
-        Expand_frames = float(
+        expand_frames = float(
             self.model.configuration["waveform_templates"]["CVACONPRO"]["expand"]
         )
         self.expected_frames = expected_frames
-        # np.ceil(expected_frames/(self.repeat_waveform*self.expand_waveform))
-        print("waveforms obtained from config")
-        print(f"Expand Frames = {Expand_frames}")
+        print(f"Expand Frames = {expand_frames}")
         print(f"Self Expected Frames test = {self.expected_frames}")
         logger.info(f"Self Expected Frames test = {self.expected_frames}")
-        logger.info(f"Expand Frames = {Expand_frames}")
+        logger.info(f"Expand Frames = {expand_frames}")
         self.model.configuration["experiment"]["MicroscopeState"][
             "number_z_steps"
         ] = expected_frames
@@ -318,7 +312,6 @@ class ConstantVelocityAcquisition:
 
         self.model.active_microscope.current_channel = 0
         self.waveform_dict = self.model.active_microscope.calculate_all_waveform()
-        # #   ms calculated v2 {self.waveform_dict}")
         self.model.active_microscope.daq.external_trigger = "/PXI6259/PFI1"
         print(
             f"external trigger set to "

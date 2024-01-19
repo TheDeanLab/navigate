66f59c5e8ffab5009b3d0825119dd869cb146f80
zacsimile <zachcm@gmail.com>
2024-01-19 18:37:56 +0100

    Fix build

--- src/navigate/__init__.py
+++ src/navigate/__init__.py
@@ -1,2 +1,8 @@
-from ._version import __version__  # noqa: F401
+import os
+from pathlib import Path
+
 from ._commit import __commit__  # noqa: F401
+
+with open(os.path.join(Path(__file__).resolve().parent, 'VERSION')) as version_file:
+    __version__ = version_file.read().strip()
+    

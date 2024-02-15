76de22ecc437f5d9120d81532b5f4521a7266403
Annie Wang <annie.xd.wang@gmail.com>
2024-02-15 14:05:25 -0800

    test live acquisition

--- test/model/test_model.py
+++ test/model/test_model.py
@@ -113,6 +113,30 @@ def test_single_acquisition(model):
     model.data_thread.join()
     model.release_pipe("show_img_pipe")
 
+def test_live_acquisition(model):
+    state = model.configuration["experiment"]["MicroscopeState"]
+    state["image_mode"] = "live"
+
+    n_images = 0
+    pre_channel = 0
+
+    show_img_pipe = model.create_pipe("show_img_pipe")
+
+    model.run_command("acquire")
+
+    while True:
+        image_id = show_img_pipe.recv()
+        if image_id == "stop":
+            break
+        channel_id = model.active_microscope.current_channel
+        assert channel_id != pre_channel
+        pre_channel = channel_id
+        n_images += 1
+        if n_images >= 30:
+            model.run_command("stop")
+    model.data_thread.join()
+    model.release_pipe("show_img_pipe")
+
 
 @pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test hangs entire workflow on GitHub.")
 def test_multiposition_acquisition(model):

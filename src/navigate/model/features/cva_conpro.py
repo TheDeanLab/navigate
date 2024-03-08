8a3af61f438f28a1915869b468f68ff5693d40dd
JinlongL <jinlong.lin@utsouthwestern.edu>
2024-03-08 15:44:15 -0600

    updated position if statement to move to correct position before starting scan.
    Need to fix move_axis_absolute command in asi stage controller. It doesn't move to correct position.

--- src/navigate/model/features/cva_conpro.py
+++ src/navigate/model/features/cva_conpro.py
@@ -335,13 +335,17 @@ class ConstantVelocityAcquisition:
         posw = self.asi_stage.get_axis_position(self.axis)
         print(f"current position = {posw}, start position = {self.start_position_um}")
         
-        if abs(posw - self.start_position_um)>10:
-            print("in posw if statement") 
-            self.asi_stage.move_axis_absolute(self.axis,self.start_position * 1000.0, wait_until_done=True)
-            print("stage moved") 
+        if abs(posw - self.start_position_um)>=2:
+            print("in posw while statement") 
+            while abs(posw - self.start_position_um)>=2:
+                print("in posw while statement") 
+                self.asi_stage.move_axis_absolute(self.axis,self.start_position_um, wait_until_done=True)
+                print("stage moved") 
+                self.asi_stage.wait_until_complete(self.axis)
+                print("Stage wait until complete completed")
+                posw = self.asi_stage.get_axis_position(self.axis)
+                print(f"current position = {posw}, start position = {self.start_position_um}")
 
-            self.asi_stage.wait_until_complete(self.axis)
-            print("Stage wait until complete completed")
 
         print(f"current position v2 = {posw}, start position v2 = {self.start_position_um}")
 

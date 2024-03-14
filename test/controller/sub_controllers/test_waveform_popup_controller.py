5569fe831e9e85486a2639026e0c1900ad2d7367
Kevin Dean <42547789+AdvancedImagingUTSW@users.noreply.github.com>
2024-03-14 11:30:54 -0500

    Update test_waveform_popup_controller.py

--- test/controller/sub_controllers/test_waveform_popup_controller.py
+++ test/controller/sub_controllers/test_waveform_popup_controller.py
@@ -1,5 +1,6 @@
 import pytest
 import random
+from unittest.mock import MagicMock
 
 
 @pytest.fixture(scope="module")
@@ -23,10 +24,14 @@ def waveform_popup_controller(dummy_view, dummy_controller):
 
 
 def test_populate_experiment_values(waveform_popup_controller):
-    exp_dict = waveform_popup_controller.parent_controller.configuration["experiment"]["MicroscopeState"]
+    exp_dict = waveform_popup_controller.parent_controller.configuration["experiment"][
+        "MicroscopeState"
+    ]
     resolution = exp_dict["microscope_name"]
     zoom = exp_dict["zoom"]
-    waveform_constants = waveform_popup_controller.parent_controller.configuration["waveform_constants"]
+    waveform_constants = waveform_popup_controller.parent_controller.configuration[
+        "waveform_constants"
+    ]
     widgets = waveform_popup_controller.view.get_widgets()
 
     def assert_widget_values():
@@ -36,7 +41,9 @@ def test_populate_experiment_values(waveform_popup_controller):
         assert widgets["Mag"].get() == zoom
 
         # remote focus
-        remote_focus_dict = waveform_constants["remote_focus_constants"][resolution][zoom]
+        remote_focus_dict = waveform_constants["remote_focus_constants"][resolution][
+            zoom
+        ]
         for k in remote_focus_dict.keys():
             assert widgets[k + " Amp"].get() == remote_focus_dict[k]["amplitude"]
             assert widgets[k + " Off"].get() == remote_focus_dict[k]["offset"]
@@ -50,11 +57,19 @@ def test_populate_experiment_values(waveform_popup_controller):
                 assert widgets[g + " Off"].get() == galvo_info["offset"]
 
         # delay, fly back time, settle duraation, smoothing
-        assert widgets["Delay"].get() == str(waveform_constants["other_constants"]["remote_focus_delay"])
-        assert widgets["Ramp_falling"].get() == str(waveform_constants["other_constants"]["remote_focus_ramp_falling"])
-        assert widgets["Duty"].get() == str(waveform_constants["other_constants"]["remote_focus_settle_duration"])
-        assert widgets["Smoothing"].get() == str(waveform_constants["other_constants"]["percent_smoothing"])
-    
+        assert widgets["Delay"].get() == str(
+            waveform_constants["other_constants"]["remote_focus_delay"]
+        )
+        assert widgets["Ramp_falling"].get() == str(
+            waveform_constants["other_constants"]["remote_focus_ramp_falling"]
+        )
+        assert widgets["Duty"].get() == str(
+            waveform_constants["other_constants"]["remote_focus_settle_duration"]
+        )
+        assert widgets["Smoothing"].get() == str(
+            waveform_constants["other_constants"]["percent_smoothing"]
+        )
+
     # default values
     waveform_popup_controller.populate_experiment_values()
     assert_widget_values()
@@ -71,7 +86,7 @@ def test_populate_experiment_values(waveform_popup_controller):
     exp_dict["zoom"] = zoom
     waveform_popup_controller.populate_experiment_values()
     assert_widget_values()
-    
+
     # update waveform_constants
     for k in waveform_constants["remote_focus_constants"][resolution][zoom].keys():
         amplitude = round(random.random() * 5, 2)
@@ -88,7 +103,12 @@ def test_populate_experiment_values(waveform_popup_controller):
         temp["amplitude"] = amplitude
         temp["offset"] = offset
 
-    for k in ["remote_focus_ramp_falling", "remote_focus_settle_duration", "percent_smoothing", "remote_focus_delay"]:
+    for k in [
+        "remote_focus_ramp_falling",
+        "remote_focus_settle_duration",
+        "percent_smoothing",
+        "remote_focus_delay",
+    ]:
         waveform_constants["other_constants"][k] = round(random.random() * 100, 2)
 
     waveform_popup_controller.populate_experiment_values(force_update=True)
@@ -103,3 +123,56 @@ def test_show_laser_info(waveform_popup_controller):
 def test_configure_widget_range(waveform_popup_controller):
     waveform_popup_controller.configure_widget_range()
     assert True
+
+
+def test_estimate_galvo_setting_empty_string(waveform_popup_controller):
+    """Test if the function returns without calling the camera setting controller."""
+    # Galvo name
+    galvo_name = "galvo_0"
+
+    # Mocked camera setting controller
+    waveform_popup_controller.parent_controller.camera_setting_controller = MagicMock()
+    waveform_popup_controller.parent_controller.camera_setting_controller.mode_widgets[
+        "Pixels"
+    ].get = MagicMock(return_value="")
+    waveform_popup_controller.parent_controller.camera_setting_controller.framerate_widgets[
+        "exposure_time"
+    ].get = MagicMock()
+
+    waveform_popup_controller.estimate_galvo_setting(galvo_name)
+    waveform_popup_controller.parent_controller.camera_setting_controller.framerate_widgets[
+        "exposure_time"
+    ].get.assert_not_called()
+
+
+def test_estimate_galvo_setting_with_string(waveform_popup_controller):
+    """Test if the function calls the camera setting controller."""
+    # Galvo name
+    galvo_name = "galvo_0"
+    number_of_pixels = 50
+
+    # Mocked camera setting controller
+    waveform_popup_controller.parent_controller.camera_setting_controller = MagicMock()
+    waveform_popup_controller.parent_controller.camera_setting_controller.mode_widgets[
+        "Pixels"
+    ].get = MagicMock(return_value=str(number_of_pixels))
+    waveform_popup_controller.parent_controller.camera_setting_controller.framerate_widgets[
+        "exposure_time"
+    ].get = MagicMock()
+
+    # Mocked model
+    waveform_popup_controller.parent_controller.model = MagicMock()
+    mock_model = waveform_popup_controller.parent_controller.model
+    mock_model.get_camera_line_interval_and_exposure_time = MagicMock(
+        return_value=(0.05, 50, 500)
+    )
+
+    # Mocked view
+    waveform_popup_controller.view = MagicMock()
+    waveform_popup_controller.view.inputs[galvo_name].widget.set = MagicMock()
+
+    # Call the function
+    waveform_popup_controller.estimate_galvo_setting(galvo_name)
+
+    # Check to see what the view was called with.
+    waveform_popup_controller.view.inputs[galvo_name].widget.set.assert_called_once()

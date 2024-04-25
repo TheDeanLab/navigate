a41879cc247df98f2a7f76aef3612d317b7d691d
Kevin Dean <42547789+AdvancedImagingUTSW@users.noreply.github.com>
2024-04-24 20:36:10 -0500

    Code Review

--- src/navigate/model/devices/stages/stage_tl_kcube_steppermotor.py
+++ src/navigate/model/devices/stages/stage_tl_kcube_steppermotor.py
@@ -51,13 +51,16 @@ import logging
 import time
 from multiprocessing.managers import ListProxy
 from numpy import round
+
+# Third Party Library imports
+
 # Local Imports
 from navigate.model.devices.stages.stage_base import StageBase
 
 
-# # Logger Setup
-# p = __name__.split(".")[1]
-# logger = logging.getLogger(p)
+# Logger Setup
+p = __name__.split(".")[1]
+logger = logging.getLogger(p)
 
 
 def build_TLKSTStage_connection(serialnum):
@@ -96,7 +99,6 @@ def build_TLKSTStage_connection(serialnum):
 class TLKSTStage(StageBase):
     """Thorlabs KST Stage"""
 
-
     def __init__(self, microscope_name, device_connection, configuration, device_id=0):
         """Initialize the stage.
 
@@ -111,18 +113,22 @@ class TLKSTStage(StageBase):
         device_id : int
             Device ID for the device.
         """
-        super().__init__(microscope_name, device_connection, configuration, device_id)  
-        
-        # only initialize the focus axis
+        super().__init__(microscope_name, device_connection, configuration, device_id)
+
+        #: dict: Mapping of axes to KST axes. Only initialize focus.
         self.axes_mapping = {"f": 1}
-        
+
         #: list: List of KST axes available.
         self.KST_axes = list(self.axes_mapping.values())
-            
-        device_config = configuration["configuration"]["microscopes"][microscope_name]["stage"]["hardware"]
+
+        device_config = configuration["configuration"]["microscopes"][microscope_name][
+            "stage"
+        ]["hardware"]
         if type(device_config) == ListProxy:
             #: str: Serial number of the stage.
             self.serial_number = str(device_config[device_id]["serial_number"])
+
+            #: float: Device units per mm.
             self.device_unit_scale = device_config[device_id]["device_units_per_mm"]
         else:
             self.serial_number = device_config["serial_number"]
@@ -134,7 +140,11 @@ class TLKSTStage(StageBase):
         else:
             self.kst_controller = build_TLKSTStage_connection(self.serial_number)
 
-    
+        logger.debug(
+            f"Connected to Thorlabs KST Stage with serial number "
+            f"{self.serial_number}"
+        )
+
     def __del__(self):
         """Delete the KST Connection"""
         try:
@@ -142,7 +152,6 @@ class TLKSTStage(StageBase):
             self.kst_controller.KST_Close(self.serial_number)
         except AttributeError:
             pass
-
 
     def report_position(self):
         """
@@ -156,10 +165,13 @@ class TLKSTStage(StageBase):
         position_dict : dict
             Dictionary containing the current position of the stage.
         """
-    
+
         try:
-            pos = self.kst_controller.KST_GetCurrentPosition(self.serial_number)/self.device_unit_scale
-            setattr(self, f"f_pos", pos)
+            pos = (
+                self.kst_controller.KST_GetCurrentPosition(self.serial_number)
+                / self.device_unit_scale
+            )
+            setattr(self, "f_pos", pos)
         except (
             self.kst_controller.TLFTDICommunicationError,
             self.kst_controller.TLDLLError,
@@ -169,14 +181,13 @@ class TLKSTStage(StageBase):
 
         return self.get_position_dict()
 
-
     def move_axis_absolute(self, axes, abs_pos, wait_until_done=False):
         """
         Implement movement.
 
         Parameters
         ----------
-        axis : str
+        axes : str
             An axis. For example, 'x', 'y', 'z', 'f', 'theta'.
         abs_pos : float
             Absolute position value
@@ -192,20 +203,24 @@ class TLKSTStage(StageBase):
         if axis_abs == -1e50:
             return False
 
-        self.kst_controller.KST_SetAbsolutePosition(self.serial_number,  int(axis_abs*self.device_unit_scale))
-        self.kst_controller.KST_MoveAbsolute(self.serial_number) 
+        self.kst_controller.KST_SetAbsolutePosition(
+            self.serial_number, int(axis_abs * self.device_unit_scale)
+        )
+        self.kst_controller.KST_MoveAbsolute(self.serial_number)
 
         if wait_until_done:
             stage_pos, n_tries, i = -1e50, 1000, 0
             target_pos = axis_abs
             while (round(stage_pos, 6) != round(target_pos, 6)) and (i < n_tries):
-                stage_pos = self.kst_controller.KST_GetCurrentPosition(self.serial_number)/self.device_unit_scale
+                stage_pos = (
+                    self.kst_controller.KST_GetCurrentPosition(self.serial_number)
+                    / self.device_unit_scale
+                )
                 i += 1
                 time.sleep(0.01)
             if stage_pos != target_pos:
                 return False
         return True
-
 
     def move_absolute(self, move_dictionary, wait_until_done=False):
         """Move stage along a single axis.
@@ -226,47 +241,52 @@ class TLKSTStage(StageBase):
         """
 
         result = True
-        result = (self.move_axis_absolute("f", move_dictionary["f_abs"], wait_until_done), result)
+        result = (
+            self.move_axis_absolute("f", move_dictionary["f_abs"], wait_until_done),
+            result,
+        )
 
         return result
 
-    
     def move_to_position(self, position, wait_until_done=False):
         """Perform a move to position
-        
+
         Parameters
         ----------
         position : float
             Stage position in mm.
         wait_until_done : bool
             Block until stage has moved to its new spot.
-        
+
         Returns
         -------
         success : bool
             Was the move successful?
         """
-        self.kst_controller.KST_MoveToPosition(self.serial_number, position*self.device_unit_scale)
-        
+        self.kst_controller.KST_MoveToPosition(
+            self.serial_number, position * self.device_unit_scale
+        )
+
         if wait_until_done:
             stage_pos, n_tries, i = -1e50, 1000, 0
             target_pos = position
             while (round(stage_pos, 4) != round(target_pos, 4)) and (i < n_tries):
-                stage_pos = self.kst_controller.KST_GetCurrentPosition(self.serial_number)/self.device_unit_scale
+                stage_pos = (
+                    self.kst_controller.KST_GetCurrentPosition(self.serial_number)
+                    / self.device_unit_scale
+                )
                 i += 1
                 time.sleep(0.01)
             if stage_pos != target_pos:
                 return False
             else:
                 return True
-            
-    
+
     def run_homing(self):
+        """Run homing sequence."""
         self.kst_controller.KST_HomeDevice(self.serial_number)
         self.move_to_position(12.5, wait_until_done=True)
-        
-    
-    
+
     def stop(self):
         """
         Stop all stage channels move

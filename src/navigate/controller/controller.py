912629c38e76e07756d57496c65378c21f94646b
Annie Wang <annie.xd.wang@gmail.com>
2025-07-29 13:55:26 -0500

    Revert "chore: move thread_pool.py to tools"
    This reverts commit c94cbebf346ecb2c84755e45ee90f41a436539a5.

--- src/navigate/controller/controller.py
+++ src/navigate/controller/controller.py
@@ -68,7 +68,7 @@ from navigate.controller.sub_controllers import (
     # AdaptiveOpticsPopupController,
 )
 
-from navigate.tools.threads import SynchronizedThreadPool
+from navigate.controller.thread_pool import SynchronizedThreadPool
 
 # Local Model Imports
 from navigate.model.model import Model

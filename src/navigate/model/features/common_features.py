f4e2d28814d7d2cf4109d72394b6cb03b0cd08f8
Annie Wang <annie.xd.wang@gmail.com>
2025-12-01 11:06:30 -0600

    add axes header to multiposition table before passing it back to the controller

--- src/navigate/model/features/common_features.py
+++ src/navigate/model/features/common_features.py
@@ -1799,7 +1799,7 @@ class FindTissueSimple2D:
             x_tiles = calc_num_tiles(xd, self.overlap, fov_x)
             y_tiles = calc_num_tiles(yd, self.overlap, fov_y)
 
-            table_values = compute_tiles_from_bounding_box(
+            axes, table_values = compute_tiles_from_bounding_box(
                 x_start,
                 x_tiles,
                 fov_x,
@@ -1822,4 +1822,4 @@ class FindTissueSimple2D:
                 self.overlap,
             )
 
-            self.model.event_queue.put(("multiposition", table_values))
+            self.model.event_queue.put(("multiposition", [axes] + table_values))

de9532f4ca9d1040f1c093fccd905396fcc63067
Annie Wang <annie.xd.wang@gmail.com>
2026-04-02 18:04:23 -0500

    set restore z and f according to primary stack stages

--- src/navigate/model/features/common_features.py
+++ src/navigate/model/features/common_features.py
@@ -1261,8 +1261,8 @@ class ZStackAcquisition:
 
         # restore z, f
         pos_dict = self.model.get_stage_position()
-        self.restore_z = pos_dict["z_pos"]
-        self.restore_f = pos_dict["f_pos"]
+        self.restore_z = pos_dict[f"{self.primary_z_axis}_pos"]
+        self.restore_f = pos_dict[f"{self.primary_f_axis}_pos"]
 
         # position: x, y, z, theta, f
         # If multiposition, get the header to know which stage is which, and then

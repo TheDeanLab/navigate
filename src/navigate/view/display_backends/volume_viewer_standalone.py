51553fb46afc7b54ddfe4132903e0ea63dd8e565
conorhughmcfadden <conorhughmcfadden@gmail.com>
2026-04-30 15:02:21 -0500

    Standalone viewer kind of working...

--- src/navigate/view/display_backends/volume_viewer_standalone.py
+++ src/navigate/view/display_backends/volume_viewer_standalone.py
@@ -1,7 +1,11 @@
 import os
 import pathlib
 import tifffile
+import numpy as np
+
 import tkinter as tk
+from tkinter import colorchooser
+
 from tkinterdnd2 import DND_FILES, TkinterDnD
 
 from navigate.view.display_backends.gl_backend import GLVolumeViewBackend
@@ -9,6 +13,19 @@ from navigate.view.custom_widgets.LabelInputWidgetFactory import LabelInput
 from navigate.view.custom_widgets.validation import ValidatedSpinbox
 
 WINDOW_DIMENSIONS = (400, 600)
+
+DEFAULT_COLORS = [
+    [255,   0,   0],
+    [  0, 255,   0],
+    [  0,   0, 255],
+    [255, 255,   0],
+    [255,   0, 255],
+    [  0, 255, 255],
+]
+
+def rgb_to_hex(color: list):
+    r, g, b = color
+    return f'#{r:02x}{g:02x}{b:02x}'
 
 class ChannelWidgetBox(tk.Frame):
     def __init__(self, master, channel_name):
@@ -32,7 +49,7 @@ class ChannelWidgetBox(tk.Frame):
                 label="Min:",
                 input_class=ValidatedSpinbox,
                 input_var=tk.IntVar(value=0),
-                input_args={"from_": 0, "to": 65535, "increment": 255, "width": 5},
+                input_args={"from_": 0, "to": 65535, "increment": 255, "width": 8},
                 label_pos="top",
             ),
             "max": LabelInput(
@@ -40,7 +57,7 @@ class ChannelWidgetBox(tk.Frame):
                 label="Min:",
                 input_class=ValidatedSpinbox,
                 input_var=tk.IntVar(value=65535),
-                input_args={"from_": 0, "to": 65535, "increment": 255, "width": 5},
+                input_args={"from_": 0, "to": 65535, "increment": 255, "width": 8},
                 label_pos="top",
             ),
         }
@@ -60,11 +77,82 @@ class VolumeViewerStandalone(TkinterDnD.Tk):
         self.main_frame = tk.Frame(self)
         self.main_frame.pack(fill=tk.BOTH, expand=True)
 
-        self.channel_widgets = {}
+class ChannelController:
 
-    def add_channel_widget(self, channel_name):
-        self.channel_widgets[channel_name] = ChannelWidgetBox(self.main_frame, channel_name)
-        self.channel_widgets[channel_name].pack(pady=5)
+    def __init__(self, parent, channel_name: str, id: int=0):
+
+        self.parent = parent
+
+        self.id = id
+        self.channel_name = channel_name
+
+        self.view = ChannelWidgetBox(parent.view.main_frame, channel_name)
+        self.view.pack(pady=5)
+
+        # Variables
+        self.color = DEFAULT_COLORS[id]
+        self.stack_data: np.ndarray = None
+        self.resolution = {'dz': 1, 'px': 1}
+        self.min = 0
+        self.max = 3000
+
+        # Widget command binds
+        inputs = self.view.inputs
+
+        inputs["color"].widget.configure(
+            command=self.choose_color,
+            bg=rgb_to_hex(self.color)
+        )
+
+    def _gl_upload_stack_to_backend(self):
+
+        self.parent.backend.set_num_slices_and_dz(
+            len(self.stack_data), self.resolution['dz']
+            )
+
+        self._gl_update_color()
+        self._gl_update_min_max()
+
+        for z, img in enumerate(self.stack_data):
+            self.parent.backend.data_q.put_nowait((img, z, self.id))
+
+    def _gl_update_color(self):
+
+        self.parent.backend.request_set_channel_color(
+            self.id,
+            [float(c)/255. for c in self.color] + [0.5] # alpha = 1 for now
+            )
+
+    def _gl_update_min_max(self):
+
+        self.parent.backend.set_min_max(
+            [self.min, self.max],
+            self.id
+        )
+
+    def load_stack(self, stack_path: str) -> np.ndarray:
+        with tifffile.TiffFile(stack_path) as tif:
+            
+            # z-spacing
+            image_desc = dict(eval(tif.pages[0].tags['ImageDescription'].value))
+            self.resolution['dz'] = image_desc['spacing']
+            
+            # xy-resolution
+            pixels, microns = tif.pages[0].tags.get('XResolution').value
+            self.resolution['px'] = microns / pixels
+        
+            # load the data
+            self.stack_data = tif.asarray()
+
+    def choose_color(self):
+
+        rgb, hex = colorchooser.askcolor(title=f"Select {self.channel_name} color...")
+
+        self.color = list(rgb)
+        self.view.inputs["color"].widget.configure(bg=hex)
+
+        # Update on GL side
+        self._gl_update_color()
 
 class VVStandaloneController:
     def __init__(self, view: TkinterDnD.Tk, backend: GLVolumeViewBackend):
@@ -74,8 +162,13 @@ class VVStandaloneController:
         self.view.main_frame.drop_target_register(DND_FILES)
         self.view.main_frame.dnd_bind('<<Drop>>', self.on_drop)
 
+        self.channels = {}
+
     def on_drop(self, event):
         dropped_files = event.data.split()
+
+        # Start GL backend
+        self.backend.start()
 
         for i, file_path in enumerate(dropped_files):
             path = pathlib.Path(file_path)
@@ -84,12 +177,14 @@ class VVStandaloneController:
 
                 channel_name = path.name.split('.')[0]
 
-                self.view.add_channel_widget(channel_name)
+                # Create a channel for this stack
+                self.channels[channel_name] = ChannelController(self, channel_name, i)
 
-                def change_color():
-                    print(f"Changing color for {channel_name}")
+                # Load the stack data/metadata into memory
+                self.channels[channel_name].load_stack(file_path)
 
-                self.view.channel_widgets[channel_name].inputs["color"].widget.configure(command=change_color)
+                # Queue a stack upload for this channel
+                self.view.after(100, self.channels[channel_name]._gl_upload_stack_to_backend)
 
 if __name__ == "__main__":
     app = VolumeViewerStandalone()
@@ -98,3 +193,7 @@ if __name__ == "__main__":
     controller = VVStandaloneController(app, gl_backend)
 
     app.mainloop()
+
+    # Close behaviour
+    if gl_backend.thread_is_running():
+        gl_backend.stop()

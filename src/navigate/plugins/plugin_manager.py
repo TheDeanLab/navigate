e90b7b1bbbf57d555b3c9a6fae1e6a26bdf55f54
Annie Wang <annie.xd.wang@gmail.com>
2026-05-22 20:43:53 -0500

    simple dependency fix: tifffile, entry_points (#1223)

--- src/navigate/plugins/plugin_manager.py
+++ src/navigate/plugins/plugin_manager.py
@@ -76,7 +76,12 @@ class PluginPackageManager:
             plugins dict
         """
         plugins = {}
-        for entry_point in entry_points().get("navigate.plugins", []):
+        eps = entry_points()
+        if hasattr(eps, 'select'):
+            navigate_eps = eps.select(group="navigate.plugins")
+        else:
+            navigate_eps = eps.get("navigate.plugins", [])
+        for entry_point in navigate_eps:
             plugin_package_name = entry_point.module.split(".")[0]
             if plugin_package_name in plugins:
                 error_statement = (
